98e6ef50e2ca514bb346d3492c3fde80b948c72a
536uriel <uribrave1@gmail.com>
2026-04-09 15:09:02 +0300

    adding read and write data file as db new  version

--- index.js
+++ index.js
@@ -3,50 +3,45 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require('path');
 var _dirname = path.resolve();
+const fs = require('node:fs/promises');
 
-var mainListArray = [
-    { "לסגור אולם": [], "done": false },
-    { " לסגור עם DJ / נגנים": [], "done": false },
-    {
-        "להחליט ולתכנן עיצוב אולם":
-            [{ "לסגור עם מעצב אולם": false }, { "לעשות סידורי ישיבה": false }], "done": false
-    },
-    { "להכין רשימת אורחים סופית עם טלפונים ושמות מלאים לתיאום הגעה": [] },
-    { "לסגור צלם + מגנטים": [], "done": false },
-    { "להכין פלייליסט לחתונה": [], "done": false },
-    { "להכין רשימת אביזרים אקסטרה לאולם": [], "done": false },
-    { "לארגן מתנות לאורחים": [], "done": false },
-    { "טעימות ובניית תפריט": [], "done": false },
-    { "סגירת סידור עם האולם": [], "done": false },
-    { "לתאם הסעות לאולם": [], "done": false },
-    { "להמציא מסמכים לפתיחת תיק רבנות": [], "done": false },
-    { " לפתוח תיק ברבנות": [], "done": false },
-    { "לסגור עם רב": [], "done": false },
-    { "לתאם הדרכות חתן וכלה": [], "done": false },
-    { "לסיים הדרכות חתן וכלה": [], "done": false },
-    { "לתאם מקווה": [], "done": false },
-    { "להזמין כתובה": [], "done": false },
-    { "להכין SAVE THE DATE": [], "done": false },
-    { " לשלוח SAVE THE DATE": [], "done": false },
-    { "לעצב הזמנות": [], "done": false },
-    { "להדפיס הזמנות": [], "done": false },
-    { "לחלק ולשלוח הזמנות": [], "done": false },
-    { "להזמין טבעות נישואין": [], "done": false },
-    { "לתאם רכב + נהג לחתונה": [], "done": false },
-    { "לדאוג לקישוט רכב": [], "done": false },
-    { "לסגור עם מאפרת+שיער": [], "done": false },
-    { " לעשות סיור שמלות": [], "done": false },
-    { "להחליט על עיצוב שמלה": [], "done": false },
-    { "למצוא ולתאם עם תופרת": [], "done": false },
-    { "לקנות חליפת חתן": [], "done": false },
-    { "לארגן אביזרים לשושבינות": [], "done": false },
-    { "לבחור שושבינות": [], "done": false },
-    { "לתאם מסיבות רווקים רווקות": [], "done": false },
-    { "לארגן מסיבת אירוסין": [], "done": false },
-    { "להסוף שמלה + נעליים": [], "done": false },
-    { "לעשות קנייה מרוכזת לאביזרים תכשיטים": [], "done": false },
-    { "להזמין/לקנות זר כלה לחתונה": [], "done": false },
-]
+async function setDataFileFromMainListArray(data) {
+    const filePath = './data.json';
+
+    try {
+
+        // --- WRITE ---
+        // JSON.stringify params: (object, replacer, space)
+        // Using '2' for the space parameter makes the file human-readable
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+        console.log('File updated successfully!');
+        return 'File updated successfully!';
+
+    } catch (error) {
+        console.error('Error handling JSON file:', error.message);
+        return 'Error handling JSON file:';
+    }
+}
+
+async function getMainListArray() {
+    const filePath = './data.json';
+
+    try {
+        // --- READ ---
+        const rawData = await fs.readFile(filePath, 'utf8');
+        const data = JSON.parse(rawData);
+        console.log('Read data:', data);
+
+        return data;
+
+
+    } catch (error) {
+        console.error('Error handling JSON file:', error.message);
+        return null;
+    }
+}
+
+
 
 
 // Use the built-in JSON body parser middleware
@@ -54,14 +49,21 @@ app.use(express.json());
 
 app.use(express.static(path.join(_dirname, 'public')))
 
-app.get("/getArray", (req, res) => {
-    res.send(JSON.stringify(mainListArray))
+app.get("/getArray", async (req, res) => {
+    let mainListArray = await getMainListArray();
+    if (mainListArray !== null) {
+        res.send(JSON.stringify(mainListArray))
+    } else {
+        res.status(500).send([]);
+    }
+
 })
 
-app.post("/update", (req, res) => {
-    mainListArray = req.body;
+app.post("/update", async (req, res) => {
+    let mainListArray = req.body;
     console.log("new rew//", mainListArray)
-    res.status(200).send({ message: "Updated successfully" });
+    let msg = await setDataFileFromMainListArray(mainListArray);
+    res.status(200).send({ message: msg });
 })
 
 app.listen(PORT, () => {
